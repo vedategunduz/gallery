2c4e5344998ce8e71ba206d9717ff1197527c2e0
vedategunduz <vedat.bilisim@outlook.com>
2023-12-10 15:31:42 +0300

    Theme color UPDATE

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -12,6 +12,7 @@ function changeLanguage(param) {
             return response.json();
         })
         .then(data => {
+            isSecureContext
             // Language
             // Skeleton
             document.getElementsByTagName("html")[0].setAttribute("lang", data.lang);
@@ -29,6 +30,24 @@ function changeLanguage(param) {
                 mobileNavItems[i].innerText = data.menu[i];
             }
             // Register Form els
+
+            if (document.getElementById("pageRegister")) {
+                // login
+                document.getElementById("login_head").innerText = data.form_els.login.login;
+                document.getElementById("login_email").setAttribute("placeholder", data.form_els.login.email);
+                document.getElementById("login_password").setAttribute("placeholder", data.form_els.login.password);
+                document.getElementById("login_remember_text").innerText = data.form_els.login.remember_me;
+                document.querySelector("#login_forgot_password span").innerText = data.form_els.login.forgot_password;
+                document.querySelector("#login_member_yet span").innerText = data.form_els.login.not_a_remember_yet;
+                document.getElementById("login_button").innerText = data.form_els.login.login;
+                // sign up
+                document.getElementById("sign_up_head").innerText = data.form_els.sign_up.sign_up;
+                document.getElementById("sign_up_username").setAttribute("placeholder", data.form_els.sign_up.username);
+                document.getElementById("sign_up_password").setAttribute("placeholder", data.form_els.sign_up.password);
+                document.getElementById("sign_up_repeat_password").setAttribute("placeholder", data.form_els.sign_up.repeat_password);
+                document.getElementById("sign_up_button").innerText = data.form_els.sign_up.sign_up;
+                document.querySelector("#sign_up_already_a_member span").innerText = data.form_els.sign_up.already_a_member;
+            }
         })
         .catch(error => {
             console.error(error);
